e32180c7b957a1b47905e78a71677ca6f8e4095c
nagendra <=>
2020-01-31 18:14:09 +0530

    added parent describes to GET and POST

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -1,77 +1,96 @@
 const request = require('supertest');
 const { app } = require('../handlers.js');
-const querystring = require('querystring');
+const sinon = require('sinon');
+const fs = require('fs');
 
-describe('GET home page', function () {
-  it('should get the home page', function (done) {
-    request(app.serve.bind(app))
-      .get('/')
-      .set('Accept', '*/*')
-      .expect(200)
-      .expect('Content-Type', 'text/html')
-      .expect('Content-Length', '911')
-      .expect(/Flower Catalog/, done);
+describe('GET', function () {
+  describe('GET home page', function () {
+    it('should get the home page', function (done) {
+      request(app.serve.bind(app))
+        .get('/')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', 'text/html')
+        .expect('Content-Length', '911')
+        .expect(/Flower Catalog/, done);
+    });
+
+    it('should get the home page when /home.html', function (done) {
+      request(app.serve.bind(app))
+        .get('/home.html')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', 'text/html')
+        .expect('Content-Length', '911')
+        .expect(/Flower Catalog/, done);
+    });
   });
 
-  it('should get the home page', function (done) {
-    request(app.serve.bind(app))
-      .get('/home.html')
-      .set('Accept', '*/*')
-      .expect(200)
-      .expect('Content-Type', 'text/html')
-      .expect('Content-Length', '911')
-      .expect(/Flower Catalog/, done);
+  describe('not found page', function () {
+    it('should get the not found page', function (done) {
+      request(app.serve.bind(app))
+        .get('/bad')
+        .set('Accept', '*/*')
+        .expect(404)
+        .expect(/Not Found/, done);
+    });
+  });
+
+  describe('guest book page', function () {
+    it('should get the not found page', function (done) {
+      request(app.serve.bind(app))
+        .get('/guest_book.html')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', 'text/html')
+        .expect(/Guest book/, done);
+    });
+  });
+
+  describe('abeliophyllum page', function () {
+    it('should get the abeliophyllum page', function (done) {
+      request(app.serve.bind(app))
+        .get('/')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', 'text/html')
+        .expect('Content-Length', '911')
+        .expect(/abeliophyllum/, done);
+    });
+  });
+
+  describe('ageratum page', function () {
+    it('should get the ageratum page', function (done) {
+      request(app.serve.bind(app))
+        .get('/')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', 'text/html')
+        .expect('Content-Length', '911')
+        .expect(/ageratum/, done);
+    });
   });
 });
 
-describe('GET not found page', function () {
-  it('should get the not found page', function (done) {
-    request(app.serve.bind(app))
-      .get('/bad')
-      .set('Accept', '*/*')
-      .expect(404)
-      .expect(/Not Found/, done);
-  });
-});
+describe('POST', function () {
+  beforeEach(() => sinon.replace(fs, 'writeFileSync', () => { }));
+  afterEach(() => sinon.restore());
 
-describe('GET  guest book page', function () {
-  it('should get the not found page', function (done) {
-    request(app.serve.bind(app))
-      .get('/guest_book.html')
-      .set('Accept', '*/*')
-      .expect(200)
-      .expect('Content-Type', 'text/html')
-      .expect(/Guest book/, done);
+  describe('/saveComment', function () {
+    it('should redirect to the guest book page', function (done) {
+      request(app.serve.bind(app))
+        .post('/saveComment')
+        .send('name=sai&&comment=good morning')
+        .expect(301, done);
+    });
   });
 
-  it('should redirect to the guest book page', function (done) {
-    request(app.serve.bind(app))
-      .post('/saveComment')
-      .send(querystring.stringify({ name: 'sai', comment: 'good morning' }))
-      .expect(301, done);
-  });
-});
-
-describe('GET abeliophyllum page', function () {
-  it('should get the abeliophyllum page', function (done) {
-    request(app.serve.bind(app))
-      .get('/')
-      .set('Accept', '*/*')
-      .expect(200)
-      .expect('Content-Type', 'text/html')
-      .expect('Content-Length', '911')
-      .expect(/abeliophyllum/, done);
-  });
-});
-
-describe('GET ageratum page', function () {
-  it('should get the ageratum page', function (done) {
-    request(app.serve.bind(app))
-      .get('/')
-      .set('Accept', '*/*')
-      .expect(200)
-      .expect('Content-Type', 'text/html')
-      .expect('Content-Length', '911')
-      .expect(/ageratum/, done);
+  describe('/bad', function () {
+    it('should redirect to the guest book page', function (done) {
+      request(app.serve.bind(app))
+        .post('/bad')
+        .send('name=sai&&comment=good morning')
+        .expect(404, done);
+    });
   });
 });
